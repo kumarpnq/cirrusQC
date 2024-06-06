98273923bbfc479bbe9ac47c557dec72c38a1cd6
sidd <siddeekshaikh97@gmail.com>
2024-06-06 20:33:17 +0530

    print statement removed

--- src/unprotected-components/Carousel.jsx
+++ src/unprotected-components/Carousel.jsx
@@ -3,7 +3,6 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ImageCarousel = ({ images }) => {
-  console.log(images);
   return (
     <Carousel showThumbs={false} infiniteLoop useKeyboardArrows>
       {images.map((image, index) => (
