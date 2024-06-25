e41dc0966f3efbd92f1624a2936f2ca783d4d1df
sidd <siddeekshaikh97@gmail.com>
2024-06-25 17:44:21 +0530

    header & tag details apis are implemented successfully

--- src/qc1-components/print/edit-dialog/EditDialog.jsx
+++ src/qc1-components/print/edit-dialog/EditDialog.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import {
   Modal,
@@ -11,6 +11,11 @@ import {
   CardContent,
   Grid,
   CircularProgress,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  Dialog,
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
@@ -24,7 +29,8 @@ import CustomTextField from "../../../@core/TextFieldWithLabel";
 import DebounceSearchCompany from "../../../@core/DebounceSearchCompany";
 import axios from "axios";
 import { url } from "../../../constants/baseUrl";
-// import { toast } from "react-toastify";
+import CustomButton from "../../../@core/CustomButton";
+import { toast } from "react-toastify";
 
 const style = {
   position: "absolute",
@@ -49,6 +55,12 @@ const titleStyle = {
 const EditDialog = ({ rowData, rowNumber, setRowNumber, open, setOpen }) => {
   const [row, setRow] = useState(null);
 
+  // * api material
+  const userToken = localStorage.getItem("user");
+  const header = {
+    Authorization: `Bearer ${userToken}`,
+  };
+
   useEffect(() => {
     const data = rowData[rowNumber] || null;
     setRow(data);
@@ -67,6 +79,45 @@ const EditDialog = ({ rowData, rowNumber, setRowNumber, open, setOpen }) => {
   const [socialFeedTagDetails, setSocialFeedTagDetails] = useState([]);
   const [socialFeedTagDetailsLoading, setSocialFeedTagDetailsLoading] =
     useState([]);
+  const [selectRowForDelete, setSelectRowForDelete] = useState({});
+  const [verificationLoading, setVerificationLoading] = useState(false);
+
+  // * fetching header & tag details
+  const fetchHeaderAndTagDetails = async () => {
+    try {
+      setSocialFeedTagDetailsLoading(true);
+      const userToken = localStorage.getItem("user");
+      const headers = {
+        Authorization: `Bearer ${userToken}`,
+      };
+
+      const headerResponse = await axios.get(
+        `${url}qc1onlineheader/?socialfeed_id=${socialFeedId}`,
+        { headers }
+      );
+      const headerData = headerResponse.data.socialfeed[0] || {};
+      setFormItems({
+        headline: headerData.headline,
+        summary: headerData.headsummary,
+        journalist: headerData.author_name,
+        tag: "",
+      });
+
+      const tagDetailsResponse = await axios.get(
+        `${url}qc1onlinetagdetails/?socialfeed_id=${socialFeedId}`,
+        { headers }
+      );
+      setSocialFeedTagDetails(tagDetailsResponse.data.socialfeed_details || []);
+    } catch (error) {
+      // toast.error("Error While fetching data.");
+      console.log(error.message);
+    } finally {
+      setSocialFeedTagDetailsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchHeaderAndTagDetails();
+  }, [socialFeedId]);
 
   const handleClose = () => {
     setRowNumber(0);
@@ -83,14 +134,146 @@ const EditDialog = ({ rowData, rowNumber, setRowNumber, open, setOpen }) => {
     });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Form Submitted: ", formItems);
-    handleClose();
+  const handleSubmit = async () => {
+    try {
+      const requestData = {
+        data: [
+          {
+            UPDATETYPE: "U",
+            SOCIALFEEDID: socialFeedId,
+            HEADLINE: formItems.headline,
+            SUMMARY: formItems.summary,
+            AUTHOR: formItems.journalist,
+            TAG: formItems.tag,
+          },
+        ],
+        qcflag: 1,
+      };
+      const response = await axios.post(
+        `${url}updatesocialfeedheader/`,
+        requestData,
+        {
+          headers: header,
+        }
+      );
+      if (response.data?.result?.success?.length) {
+        if (rowNumber < rowData.length - 1) {
+          setRowNumber((prev) => prev + 1);
+        } else {
+          toast.success("This is the last article.");
+          handleClose();
+        }
+      } else {
+        const errorMSG = response.data?.result?.errors[0] || {};
+        toast.warning(errorMSG.warning);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const handleDeleteCompany = (row) => {
-    console.log(row);
+  const handleAddCompany = async () => {
+    try {
+      const requestData = {
+        data: [
+          {
+            UPDATETYPE: "I",
+            SOCIALFEEDID: socialFeedId,
+            COMPANYID: selectedCompanies.value,
+            COMPANYNAME: selectedCompanies.label,
+          },
+        ],
+        qcflag: 1,
+      };
+      const response = await axios.post(
+        `${url}updatesocialfeedtagdetails/`,
+        requestData,
+        {
+          headers: header,
+        }
+      );
+      if (response.status === 200) {
+        fetchHeaderAndTagDetails();
+        toast.success("Company added.");
+      }
+    } catch (error) {
+      console.log("Something went wrong");
+    }
+  };
+
+  const userVerification = async () => {
+    try {
+      setVerificationLoading(true);
+      const headers = { Authorization: `Bearer ${userToken}` };
+      const data = { password };
+      const response = await axios.post(`${url}isValidUser/`, data, {
+        headers,
+      });
+      setVerificationLoading(false);
+      return response.data.valid_user;
+    } catch (error) {
+      console.log("Error:", error.message);
+      setVerificationLoading(false);
+    }
+  };
+
+  // * delete dialog
+  const [openDelete, setOpenDelete] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleCloseDelete = () => {
+    setOpenDelete(false);
+  };
+
+  const handleDeleteClick = (rowData) => {
+    setSelectRowForDelete(rowData);
+    setOpenDelete(true);
+  };
+
+  const handleDeleteCompany = async () => {
+    const isValid = await userVerification();
+    if (!isValid) {
+      return toast.error("Password not match with records");
+    }
+    const selectedRow = socialFeedTagDetails.find(
+      (i) => i.company_name === selectRowForDelete.CompanyName
+    );
+
+    if (!selectedRow) {
+      return toast.warning("No Company match");
+    }
+    try {
+      const requestData = {
+        data: [
+          {
+            UPDATETYPE: "D",
+            SOCIALFEEDID: socialFeedId,
+            COMPANYID: selectedRow.company_id,
+            COMPANYNAME: selectedRow.company_name,
+          },
+        ],
+        qcflag: 1,
+      };
+      const response = await axios.post(
+        `${url}updatesocialfeedtagdetails/`,
+        requestData,
+        {
+          headers: header,
+        }
+      );
+
+      if (response.data?.result?.success?.length) {
+        fetchHeaderAndTagDetails();
+        toast.success("Company removed");
+        setPassword("");
+        setOpenDelete(false);
+      } else {
+        const errorMSG = response.data?.result?.error[0] || {};
+        toast.warning(errorMSG.error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const columns = [
@@ -101,7 +284,7 @@ const EditDialog = ({ rowData, rowNumber, setRowNumber, open, setOpen }) => {
       renderCell: (params) => (
         <IconButton
           sx={{ color: "red" }}
-          onClick={() => handleDeleteCompany(params.row)}
+          onClick={() => handleDeleteClick(params.row)}
         >
           <CloseIcon />
         </IconButton>
@@ -115,189 +298,187 @@ const EditDialog = ({ rowData, rowNumber, setRowNumber, open, setOpen }) => {
     CompanyName: detail.company_name,
   }));
 
-  // * fetching header & tag details
-
-  useEffect(() => {
-    const fetchHeaderAndTagDetails = async () => {
-      try {
-        setSocialFeedTagDetailsLoading(true);
-        const userToken = localStorage.getItem("user");
-        const headers = {
-          Authorization: `Bearer ${userToken}`,
-        };
-
-        const headerResponse = await axios.get(
-          `${url}qc1onlineheader/?socialfeed_id=${socialFeedId}`,
-          { headers }
-        );
-        const headerData = headerResponse.data.socialfeed[0] || {};
-        setFormItems({
-          headline: headerData.headline,
-          summary: headerData.headsummary,
-          journalist: headerData.author_name,
-          tag: "",
-        });
-
-        const tagDetailsResponse = await axios.get(
-          `${url}qc1onlinetagdetails/?socialfeed_id=${socialFeedId}`,
-          { headers }
-        );
-        setSocialFeedTagDetails(tagDetailsResponse.data.socialfeed_details);
-      } catch (error) {
-        // toast.error("Error While fetching data.");
-        console.log(error.message);
-      } finally {
-        setSocialFeedTagDetailsLoading(false);
-      }
-    };
-
-    fetchHeaderAndTagDetails();
-  }, [socialFeedId]);
-
   return (
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="edit-dialog-title"
-      aria-describedby="edit-dialog-description"
-    >
-      <Box sx={style}>
-        <Box sx={titleStyle}>
-          <Typography
-            id="edit-dialog-title"
-            variant="h6"
-            component="h6"
-            fontSize={"1em"}
-          >
-            Edit
-          </Typography>
-          <Typography id="edit-dialog-description" component={"div"}>
-            <Button variant="outlined">Save & Next</Button>
-            <IconButton onClick={handleClose}>
-              <CloseOutlined />
-            </IconButton>
-          </Typography>
-        </Box>
+    <Fragment>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="edit-dialog-title"
+        aria-describedby="edit-dialog-description"
+      >
+        <Box sx={style}>
+          <Box sx={titleStyle}>
+            <Typography
+              id="edit-dialog-title"
+              variant="h6"
+              component="h6"
+              fontSize={"1em"}
+            >
+              Edit
+            </Typography>
+            <Typography id="edit-dialog-description" component={"div"}>
+              <Button variant="outlined" onClick={handleSubmit}>
+                Save & Next
+              </Button>
+              <IconButton onClick={handleClose}>
+                <CloseOutlined />
+              </IconButton>
+            </Typography>
+          </Box>
 
-        <Card>
-          {" "}
-          <CardHeader
-            title={<Typography component={"span"}>Basic Details</Typography>}
-          />
-          <CardContent>
-            <Box component="form" onSubmit={handleSubmit}>
-              <Grid container spacing={1}>
-                <Grid item xs={12} sm={6}>
-                  <CustomTextField
-                    id="headline"
-                    name="headline"
-                    label="Headline"
-                    value={formItems.headline}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <CustomTextField
-                    id="summary"
-                    name="summary"
-                    label="Summary"
-                    value={formItems.summary}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <CustomTextField
-                    id="journalist"
-                    name="journalist"
-                    label="Journalist"
-                    value={formItems.journalist}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <CustomTextField
-                    id="tag"
-                    name="tag"
-                    label="Tag"
-                    value={formItems.tag}
-                    onChange={handleChange}
-                  />
-                </Grid>
-              </Grid>
-            </Box>
-          </CardContent>
-        </Card>
-        <Grid container spacing={1} mt={1}>
-          <Grid item xs={12} sm={6}>
-            <Card>
-              <CardHeader
-                title={
-                  <Typography component={"span"}>Tagged Companies</Typography>
-                }
-              />
-              <CardContent>
-                <Box sx={{ display: "flex", justifyContent: "center" }}>
-                  <DebounceSearchCompany
-                    setSelectedCompany={setSelectedCompanies}
-                  />
-                  <Button>Add</Button>
-                </Box>
-                <Box height={500} width={"100%"}>
-                  <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    density="compact"
-                    loading={
-                      socialFeedTagDetailsLoading && <CircularProgress />
-                    }
-                    pageSize={5}
-                    pageSizeOptions={[10, 100, 200, 1000]}
-                    columnBufferPx={1000}
-                    hideFooterSelectedRowCount
-                  />
-                </Box>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Card>
-              <CardHeader
-                title={
-                  <Typography
-                    variant="h6"
-                    component={"a"}
-                    display="flex"
-                    alignItems="center"
-                    gap={1}
-                    fontSize={"0.9em"}
-                    href={iframeURI}
-                    target="_blank"
-                    rel="noreferrer"
-                    fontFamily="nunito"
-                    className="underline text-primary"
-                  >
-                    Article View
-                    <FaExternalLinkAlt
-                      style={{
-                        fontSize: "1.2em",
-                        fontFamily: "nunito",
-                      }}
+          <Card>
+            {" "}
+            <CardHeader
+              title={<Typography component={"span"}>Basic Details</Typography>}
+            />
+            <CardContent>
+              <Box component="form">
+                <Grid container spacing={1}>
+                  <Grid item xs={12} sm={6}>
+                    <CustomTextField
+                      id="headline"
+                      name="headline"
+                      label="Headline"
+                      value={formItems.headline}
+                      onChange={handleChange}
                     />
-                  </Typography>
-                }
-              />
-              <CardContent>
-                <iframe
-                  src={iframeURI}
-                  frameBorder="0"
-                  style={{ width: "100%", height: "540px" }}
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <CustomTextField
+                      id="summary"
+                      name="summary"
+                      label="Summary"
+                      value={formItems.summary}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <CustomTextField
+                      id="journalist"
+                      name="journalist"
+                      label="Journalist"
+                      value={formItems.journalist}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <CustomTextField
+                      id="tag"
+                      name="tag"
+                      label="Tag"
+                      value={formItems.tag}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+            </CardContent>
+          </Card>
+          <Grid container spacing={1} mt={1}>
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <CardHeader
+                  title={
+                    <Typography component={"span"}>Tagged Companies</Typography>
+                  }
                 />
-              </CardContent>
-            </Card>
+                <CardContent>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <DebounceSearchCompany
+                      setSelectedCompany={setSelectedCompanies}
+                    />
+                    <Button onClick={handleAddCompany}>Add</Button>
+                  </Box>
+                  <Box height={500} width={"100%"}>
+                    <DataGrid
+                      rows={rows}
+                      columns={columns}
+                      density="compact"
+                      loading={
+                        socialFeedTagDetailsLoading && <CircularProgress />
+                      }
+                      pageSize={5}
+                      pageSizeOptions={[10, 100, 200, 1000]}
+                      columnBufferPx={1000}
+                      hideFooterSelectedRowCount
+                    />
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <CardHeader
+                  title={
+                    <Typography
+                      variant="h6"
+                      component={"a"}
+                      display="flex"
+                      alignItems="center"
+                      gap={1}
+                      fontSize={"0.9em"}
+                      href={iframeURI}
+                      target="_blank"
+                      rel="noreferrer"
+                      fontFamily="nunito"
+                      className="underline text-primary"
+                    >
+                      Article View
+                      <FaExternalLinkAlt
+                        style={{
+                          fontSize: "1.2em",
+                          fontFamily: "nunito",
+                        }}
+                      />
+                    </Typography>
+                  }
+                />
+                <CardContent>
+                  <iframe
+                    src={iframeURI}
+                    frameBorder="0"
+                    style={{ width: "100%", height: "540px" }}
+                  />
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
-    </Modal>
+        </Box>
+      </Modal>
+      <Dialog open={openDelete} onClose={handleCloseDelete}>
+        <DialogTitle fontSize={"1em"}>
+          Enter Password For Confirmation.
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            type="password"
+            sx={{ outline: "none" }}
+            size="small"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <CustomButton
+            btnText="Cancel"
+            onClick={handleCloseDelete}
+            bg={"bg-primary"}
+          />
+          {verificationLoading ? (
+            <Box width={100}>
+              <CircularProgress />
+            </Box>
+          ) : (
+            <CustomButton
+              btnText="Delete"
+              onClick={handleDeleteCompany}
+              bg={"bg-red-500"}
+            />
+          )}
+        </DialogActions>
+      </Dialog>
+    </Fragment>
   );
 };
 
